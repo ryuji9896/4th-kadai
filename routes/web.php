981b7d0888cfd28a1956b358ed161fcd1a24c892
ryuji9896 <makinoryujijoker9896@i.softbank.jp>
2023-12-19 14:25:38 +0000

    laravel9-課題追加

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function(){
+    Route::get('news/create','add');
+});
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function() { 
+    Route::prefix('Profile')->group(function(){
+        Route::get('','add');
+        Route::get('','edit');
+    });
+});
