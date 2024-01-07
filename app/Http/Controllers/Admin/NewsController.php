671587a14d22c7335994b8a60170b01e897a04fe
EC2 Default User <ec2-user@ip-172-31-36-197.ap-southeast-2.compute.internal>
2024-01-07 10:14:25 +0000

    laravel17テキスト内容追加

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\News;
+use App\Models\History;
+use Carbon\Carbon;
 
 class NewsController extends Controller
 {
@@ -75,6 +77,11 @@ class NewsController extends Controller
         unset($news_form['image']);
         
         $news->fill($news_form)->save();
+        
+        $history = new History();
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
         
         return redirect('admin/news');
     }
