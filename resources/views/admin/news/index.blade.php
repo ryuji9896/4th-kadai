915c28583a7108237b684069c77f382229b30496
EC2 Default User <ec2-user@ip-172-31-36-197.ap-southeast-2.compute.internal>
2024-01-08 14:59:58 +0000

    index画面のカラム毎の割合部分を修正

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -30,10 +30,10 @@
                         <table class="table table-dark">
                             <thead>
                                 <tr>
-                                    <th width"10%">ID</th>
-                                    <th width"20%">タイトル</th>
-                                    <th width"50%">本文</th>
-                                    <th width"10%">操作</th>
+                                    <th width="10%">ID</th>
+                                    <th width="20%">タイトル</th>
+                                    <th width="50%">本文</th>
+                                    <th width="10%">操作</th>
                                 </tr>
                             </thead>
                             <tbody>
