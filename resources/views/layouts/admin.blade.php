64a25ad86734ca211e098157cb472b46b20e49b3
EC2 Default User <ec2-user@ip-172-31-36-197.ap-southeast-2.compute.internal>
2024-01-20 12:41:55 +0000

    閲覧者用画面に投稿画面へのリンクを追加

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,6 +24,9 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
+                    <a class="navbar-brand" href="{{ url('/admin/news/') }}">
+                        {{ config('app.name', 'postcreate') }}
+                    </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
