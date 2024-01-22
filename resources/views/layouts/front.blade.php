d6dd242a0ee540fcbda12adf07eabc54d97a1bcf
ryuji9896 <makinoryujijoker9896@i.softbank.jp>
2024-01-22 13:02:31 +0000

    ローカルサーバー用を編集するように修正

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -24,9 +24,6 @@
                     <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
-                    <a class="navbar-brand" href="{{ url('/admin/news/') }}">
-                        {{ config('app.postcreate', 'postcreate') }}
-                    </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
